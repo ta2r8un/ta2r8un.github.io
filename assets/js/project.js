be92bceb18818898c9037dc7840a1a4969b79299
ta2r8un <tarunsehdev19@gmail.com>
2025-07-20 17:29:04 +0530

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -6,7 +6,7 @@ const projectcards = document.querySelector(".projectcards");
 // Array of object for projects
 const projects = [
   {
-    title: "Quiz App",
+    title: "earning app",
     cardImage: "assets/images/project-page/quiz.jpg",
     description: "A quiz app built using basic react.",
     tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
